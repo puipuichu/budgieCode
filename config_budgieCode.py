25897009ae2a6f5e5eaf3e52c9c46f8df49d257d
puipuichu <147630833+puipuichu@users.noreply.github.com>
2024-02-09 17:11:47 +0100

    Add files via upload

---
+++ config_budgieCode.py
@@ -0,0 +1,29 @@
+##########
+# Config #
+##########
+
+# Set program folder path
+folderPath = "C:/Users/pchu/Documents/preferenceApparatus/Distortion_Clean/"
+
+# Set folder path to save data
+logFolder = folderPath + "LOGS/"
+
+# Set arduino port
+arduinoPort = "COM4"  
+
+# Set folder path to stimulus
+song_folderPath = folderPath + "stimulus/"
+
+# Set folder path to stimulus A and stimulus B
+stimA_folderPath = song_folderPath + "ran/"
+stimB_folderPath = song_folderPath + "reg/"
+
+# Set name of stimulus A and stimulus B
+stimA = "ran"
+stimB = "reg"
+
+# Set number of stimulus in each folder
+stim_no = 5
+
+# Set stimulus trigger threshold: after bird on perch for ... seconds
+threshold_time = 0.3
